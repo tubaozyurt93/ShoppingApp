4a4a4fcba50b593e51e83aff7831b003fe808f18
necla neslitürk <54413709+Necla-1995@users.noreply.github.com>
2021-12-15 01:05:17 +0300

    Update NavbarComp.js

--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -1,46 +1,45 @@
 import React, { Component } from 'react';
-import {Navbar, Nav, NavDropdown, Form, FormControl, Button, Container} from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap';
 
 export default class NavbarComp extends Component {
     render() {
         return (
             <div>
                 <Navbar bg="dark" variant='dark' expand="lg">
-  <Container fluid>
-    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbarScroll" />
-    <Navbar.Collapse id="navbarScroll">
-      <Nav
-        className="me-auto my-2 my-lg-0"
-        style={{ maxHeight: '100px' }}
-        navbarScroll
-      >
-        <Nav.Link href="#action1">Home</Nav.Link>
-        <Nav.Link href="#action2">Link</Nav.Link>
-        <NavDropdown title="Link" id="navbarScrollingDropdown">
-          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action5">
-            Something else here
-          </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#" disabled>
-          Link
-        </Nav.Link>
-      </Nav>
-      <Form className="d-flex">
-        <FormControl
-          type="search"
-          placeholder="Search"
-          className="me-2"
-          aria-label="Search"
-        />
-        <Button variant="outline-success">Search</Button>
-      </Form>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+                    <Container fluid>
+                        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-lg-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <NavDropdown title="Men's Clothing" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item href="#action3">T-Shirts</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action4">Jacket</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action5">Bag</NavDropdown.Item>
+                                </NavDropdown>
+                                <NavDropdown title="Women's Clothing" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item href="#action3">T-Shirts</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action4">Jacket</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action4">Jewelery</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action5">Bag</NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="#action1">Electronics</Nav.Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <FormControl
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
             </div>
         )
     }
